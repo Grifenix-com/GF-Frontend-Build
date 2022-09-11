90d27d45e9c4ff04043bdc68c169badc4143be74
Vladyslav <vlama@inf.upv.es>
2022-09-12 01:45:55 +0200

    Adding the pwd update

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb06e9447161b1ff9df3de8941c22324.js"
+  "/precache-manifest.5dff2f689386b69878937de4f1df3fcc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "grifenix"});
